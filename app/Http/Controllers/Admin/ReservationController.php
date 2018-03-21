1787777b3d11f37d0b199787a0d0b1e53ed62cb7
Md Aminul Islam Fahim <cipfahim1999@gmail.com>
2018-03-21 22:45:23 +0600

    reservation notification added

--- app/Http/Controllers/Admin/ReservationController.php
+++ app/Http/Controllers/Admin/ReservationController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Notifications\ReservationConfirmed;
 use App\Reservation;
 use Illuminate\Http\Request;
 use Brian2694\Toastr\Facades\Toastr;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Notification;
 
 class ReservationController extends Controller
 {
@@ -18,6 +20,8 @@ class ReservationController extends Controller
         $reservation = Reservation::find($id);
         $reservation->status = true;
         $reservation->save();
+        Notification::route('mail',$reservation->email )
+            ->notify(new ReservationConfirmed());
         Toastr::success('Reservation successfully confirmed.','Success',["positionClass" => "toast-top-right"]);
         return redirect()->back();
     }
